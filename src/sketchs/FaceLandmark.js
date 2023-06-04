5152ccacde30ec17776e9f89305f5d902374d920
Ronnasayd <ronnasayd@hotmail.com>
2023-06-04 00:57:10 -0300

    refactor: add indexes

--- src/sketchs/FaceLandmark.js
+++ src/sketchs/FaceLandmark.js
@@ -6,8 +6,17 @@ const v = factoryProxy({
   CANVAS_HEIGHT: 600,
   CANVAS_WIDTH: 600,
   fps: 12,
-  videoCapture: null,
-  landmarks: null,
+  videoCapture: new p5.Element("video"),
+  canvas: new p5.Element("canvas"),
+  landmarks: [],
+  point: null,
+  landmark_points_68: [
+    162, 234, 93, 58, 172, 136, 149, 148, 152, 377, 378, 365, 397, 288, 323,
+    454, 389, 71, 63, 105, 66, 107, 336, 296, 334, 293, 301, 168, 197, 5, 4, 75,
+    97, 2, 326, 305, 33, 160, 158, 133, 153, 144, 362, 385, 387, 263, 373, 380,
+    61, 39, 37, 0, 267, 269, 291, 405, 314, 17, 84, 181, 78, 82, 13, 312, 308,
+    317, 14, 87,
+  ],
 });
 
 /**
@@ -21,8 +30,8 @@ const script = function (p5) {
     p5.frameRate(v.fps);
     v.videoCapture = p5.createCapture(p5.VIDEO, async (stream) => {
       await FaceLandmarkDetection.init("VIDEO");
-      v.CANVAS_WIDTH = v.videoCapture.width;
-      v.CANVAS_HEIGHT = v.videoCapture.height;
+      v.CANVAS_WIDTH = v.videoCapture?.width;
+      v.CANVAS_HEIGHT = v.videoCapture?.height;
       v.canvas = p5.createCanvas(v.videoCapture.width, v.videoCapture.height);
       v.canvas.addClass("absolute z-[1]");
     });
@@ -36,10 +45,15 @@ const script = function (p5) {
       v.videoCapture.elt
     )?.faceLandmarks?.[0];
 
-    p5.fill("#00ff99");
-    if (v.landmarks) {
-      for (const landmark of v.landmarks) {
-        p5.circle(landmark.x * v.CANVAS_WIDTH, landmark.y * v.CANVAS_HEIGHT, 1);
+    if (!!v.landmarks?.length) {
+      for (let index = 0; index < v.landmarks.length; index++) {
+        v.point = v.landmarks[index];
+        if (v.landmark_points_68.includes(index)) {
+          p5.fill("#00ff00");
+          p5.circle(v.point.x * v.CANVAS_WIDTH, v.point.y * v.CANVAS_HEIGHT, 4);
+        } else {
+          p5.circle(v.point.x * v.CANVAS_WIDTH, v.point.y * v.CANVAS_HEIGHT, 1);
+        }
       }
     }
   };
---
+++ src/sketchs/FaceLandmarkImageKeypoints.js
@@ -0,0 +1,105 @@
+import { Delaunay } from "d3-delaunay";
+import p5 from "p5";
+import { factoryProxy } from "../common";
+import { FaceLandmarkDetection } from "../common/MediaPipeCommon";
+
+const v = factoryProxy({
+  CANVAS_WIDTH: 550,
+  CANVAS_HEIGHT: 550,
+  fps: 30,
+  refImg: null,
+  canvas: null,
+  getTriangles: function () {
+    const triangles = [];
+    for (
+      let index = 0;
+      index < this.delaunay.triangles.length;
+      index = index + 3
+    ) {
+      const t0 = this.delaunay.triangles[index];
+      const t1 = this.delaunay.triangles[index + 1];
+      const t2 = this.delaunay.triangles[index + 2];
+
+      const p0x = this.delaunay.points[t0 * 2];
+      const p0y = this.delaunay.points[t0 * 2 + 1];
+
+      const p1x = this.delaunay.points[t1 * 2];
+      const p1y = this.delaunay.points[t1 * 2 + 1];
+
+      const p2x = this.delaunay.points[t2 * 2];
+      const p2y = this.delaunay.points[t2 * 2 + 1];
+
+      triangles.push([p0x, p0y, p1x, p1y, p2x, p2y]);
+    }
+    return triangles;
+  },
+  getKeypoints: function () {
+    const points = [];
+    const map = {};
+    for (const index of v.landmark_points_68) {
+      points.push([
+        this.faceLandmarks[index].x * v.CANVAS_WIDTH,
+        this.faceLandmarks[index].y * v.CANVAS_HEIGHT,
+      ]);
+      map[
+        `${this.faceLandmarks[index].x * v.CANVAS_WIDTH}:${
+          this.faceLandmarks[index].y * v.CANVAS_HEIGHT
+        }`
+      ] = index;
+    }
+
+    return [points, map];
+  },
+  faceLandmarks: [],
+  delaunay: [],
+  landmark_points_68: [
+    162, 234, 93, 58, 172, 136, 149, 148, 152, 377, 378, 365, 397, 288, 323,
+    454, 389, 71, 63, 105, 66, 107, 336, 296, 334, 293, 301, 168, 197, 5, 4, 75,
+    97, 2, 326, 305, 33, 160, 158, 133, 153, 144, 362, 385, 387, 263, 373, 380,
+    61, 39, 37, 0, 267, 269, 291, 405, 314, 17, 84, 181, 78, 82, 13, 312, 308,
+    317, 14, 87,
+  ],
+});
+
+/**
+ * @param {p5} p5
+ */
+const script = function (p5) {
+  p5.createFileInput(async function (file) {
+    v.refImg = p5.createImg(file.data);
+    v.refImg.addClass("absolute z-[0] h-[550px]");
+    v.refImg.elt.onload = async (e) => {
+      v.CANVAS_WIDTH = (v.refImg.width / v.refImg.height) * v.CANVAS_HEIGHT;
+      v.canvas = p5.createCanvas(v.CANVAS_WIDTH, v.CANVAS_HEIGHT);
+      v.canvas.addClass("absolute z-[1]");
+      v.faceLandmarks = (
+        await FaceLandmarkDetection.detectForImage(v.refImg.elt)
+      ).faceLandmarks[0];
+    };
+  });
+  p5.setup = async () => {
+    await FaceLandmarkDetection.init("IMAGE");
+    p5.frameRate(v.fps);
+  };
+  p5.draw = () => {
+    p5.background(255, 0);
+    if (!!v.faceLandmarks.length) {
+      const [points, map] = v.getKeypoints();
+      v.delaunay = Delaunay.from(points);
+
+      p5.noFill();
+      p5.stroke(0, 255, 50, 1);
+      p5.strokeWeight(2);
+      for (const triangle of v.getTriangles()) {
+        p5.triangle(...triangle);
+      }
+
+      p5.fill("#ff0000");
+      for (const point of points) {
+        p5.circle(point[0], point[1], 4);
+        p5.text(map[`${point[0]}:${point[1]}`], point[0], point[1]);
+      }
+    }
+  };
+};
+new p5(script);
